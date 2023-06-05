0c95bd598cd1a0573a082ec30033ad784faf0d66
coriawork <manuelcoriawork@gmail.com>
2023-06-04 22:12:04 -0300

    ,

--- 2.Corte de control/corteControl.pas
+++ 2.Corte de control/corteControl.pas
@@ -1,5 +1,8 @@
 program corteControl;
 {
+    El archivo se encuentra ordenado por provincia, partido y ciudad
+    ejem
+
     |******************************|
     |Provincia: xxxx               |
     |------------------------------|
@@ -75,24 +78,30 @@ begin
     while ( regm.provincia <> valoralto)do begin 
         ant_prov := regm.provincia; 
         ant_partido := regm.partido;
+        // Mientras es la misma provincia y el mismo partido
         while (ant_prov=regm.provincia) and (ant_partido=regm.partido) do begin
             write (regm.ciudad, regm.cant_varones,regm.cant_mujeres,regm.cant_desocupados);
+            //Sumo los valores
             t_varones := t_varones + regm.cant_varones;      
             t_mujeres := t_mujeres + regm.cant_mujeres;
             t_desocupa := t_desocupa + regm.cant_desocupados;
             leer (inst, regm);
         end;
+        // Escribo el total del partido 
         writeln ('Total Partido: ', t_varones, t_mujeres, t_desocupa);
+        // Sumo el total de las provincias
         t_prov_var := t_prov_var + t_varones; 
         t_prov_muj := t_prov_muj +  t_mujeres;  
         t_prov_des := t_prov_des + t_desocupa;
+        // Vuelvo todos a 0
         t_varones := 0; t_mujeres := 0; t_desocupa := 0;
         
+        // Igualo el partido
         ant_partido := regm.partido;
-
+        // Si la provincia es ditinta
         if (ant_prov <> regm.provincia) then begin
-            writeln ('TotalProv.',t_prov_var, 
-            t_prov_muj, t_prov_des);
+            // Escribo los totales de la provincia
+            writeln ('TotalProv.',t_prov_var, t_prov_muj, t_prov_des);
             t_prov_var := 0; t_prov_muj := 0; 
             t_prov_des := 0;
             writeln('Prov.:',regm.provincia);
